c636b105fa6a13695f92b472aca6e48c8ccdab9f
KhoiMM <minhkhoi09091999@gmail.com>
2024-03-10 12:57:37 +0700

    first commit

---
+++ src/app/location.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { WeatherService } from "./weather.service";
+
+export const LOCATIONS: string = "locations";
+
+@Injectable()
+export class LocationService {
+  locations: string[] = [];
+
+  constructor(private weatherService: WeatherService) {
+    console.log(11111);
+    let locString = localStorage.getItem(LOCATIONS);
+    if (locString) this.locations = JSON.parse(locString);
+    for (let loc of this.locations)
+      this.weatherService.addCurrentConditions(loc);
+  }
+
+  addLocation(zipcode: string) {
+    this.locations.push(zipcode);
+    localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
+    this.weatherService.addCurrentConditions(zipcode);
+  }
+
+  removeLocation(zipcode: string) {
+    let index = this.locations.indexOf(zipcode);
+    if (index !== -1) {
+      this.locations.splice(index, 1);
+      localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
+      this.weatherService.removeCurrentConditions(zipcode);
+    }
+  }
+}
